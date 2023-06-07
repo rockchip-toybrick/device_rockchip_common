c60eda2f26345436cd9471580a163e558a18eec3
dawnming.huang <dawnming.huang@rock-chips.com>
2023-06-07 15:56:10 +0800

    common: audio: upgrade car_audio xml files
    Change-Id: I19f0134e119cdfd9338bb4a7be6e4e27c1adf757
    Signed-off-by: dawnming.huang <dawnming.huang@rock-chips.com>

--- device.mk
+++ device.mk
@@ -187,7 +187,9 @@ PRODUCT_PACKAGES += \
     audio.hdmi_1.$(TARGET_BOARD_HARDWARE)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/car_audio_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/car_audio_configuration.xml \
-    $(LOCAL_PATH)/car_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/car_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/car_audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/car_default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 else
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
